5f695ec88f257c820cbb74ad869bfcfba410c594
lucas <lucasgonzalez5500@gmail.com>
2024-05-27 12:00:51 -0300

    .

--- routes/api.php
+++ routes/api.php
@@ -11,6 +11,8 @@ use Illuminate\Support\Facades\Route;
 // Route::middleware(['set.user.database', 'auth:sanctum'])->group(function() {
 Route::middleware(['auth:sanctum'])->group(function() {
 
+    // HOLA
+
     // CommonLaravel 
     // ----------------------------------------------------------------------------------------------------
     // Generals
